8e16942595f6d77b55673735aa9425f53992a505
shivangi <shivangi.jain@tothenew.com>
2020-05-10 16:26:48 +0530

    Added a comment to specify multiple consumers

--- src/main/java/com/rabbitMQ/maven/app/Consumer2.java
+++ src/main/java/com/rabbitMQ/maven/app/Consumer2.java
@@ -8,6 +8,8 @@ import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+//For creating multiple consumers.
+
 public class Consumer2 {
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
